e4ab5349c872d91478db10fea9e04a8b09d4e767
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2021-05-24 18:48:17 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int bsearch (const vector<int> &A ) {
+    int inicio = 0, final = n;
+    while ( final−inicio >1) {
+        int mid = ( inicio+final ) /2;
+        if ( A[mid] <= A[mid+1] ) {
+            inicio = mid;
+        } else {
+            final = mid;
+        }
+    }
+    return final ;
+}
+
